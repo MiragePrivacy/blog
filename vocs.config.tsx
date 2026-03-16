4b883490c081b33533a0c575b782e9c684a6aa94
g4titanx <g4titan1@gmail.com>
2026-03-16 14:32:13 +0100

    feat: integrate vocs

---
+++ vocs.config.tsx
@@ -0,0 +1,57 @@
+import { defineConfig } from "vocs";
+
+export default defineConfig({
+  basePath: process.env.BASE_PATH || undefined,
+  title: "Mirage Blog",
+  logoUrl: "/logo.svg",
+  iconUrl: "/logo.svg",
+  blogDir: "./pages/posts",
+  socials: [
+    {
+      icon: "x",
+      link: "https://x.com/mirageprivacy",
+    },
+    {
+      icon: "discord",
+      link: "https://discord.gg/FCcKwPyBxV",
+    },
+    {
+      icon: "github",
+      link: "https://github.com/MiragePrivacy",
+    },
+  ],
+  topNav: [
+
+    {
+      text: "Docs",
+      link: "https://docs.mirageprivacy.com",
+    },
+    {
+      text: "App",
+      link: "https://app.mirageprivacy.com",
+    },
+  ],
+  sidebar: {
+    "/posts/": [
+      {
+        text: "Navigation",
+        items: [
+          { text: "Home", link: "/" },
+          { text: "All Posts", link: "/posts" },
+        ],
+      },
+    ],
+  },
+  head() {
+    return (
+      <>
+        <meta name="twitter:card" content="summary" />
+        <meta property="og:title" content="Mirage Blog" />
+        <meta
+          property="og:description"
+          content="Dispatches from the frontier of on-chain privacy."
+        />
+      </>
+    );
+  },
+});
